e7ba76fed556a08fc45efed314f5851b4b274988
Peter Huang <91788314+koalazodiac@users.noreply.github.com>
2024-12-13 14:25:48 -0500

    Delete assignment2_new2 directory

--- assignment2_new2/src/assignment2/EvolutionStage.java
+++
@@ -1,8 +0,0 @@
-package assignment2;
-
-public enum EvolutionStage {
-	ENTANGLED,
-	FEEDING_STAGE,
-	GROWING_STAGE,
-	BUTTERFLY
-}
--- assignment2_new2/src/assignment2/food/FoodItem.java
+++
@@ -1,9 +0,0 @@
-package assignment2.food;
-
-import assignment2.Caterpillar;
-
-public abstract class FoodItem {
-
-	public abstract void accept(Caterpillar c);
-	
-}
